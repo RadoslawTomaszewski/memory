6663deb6c9a0ceaccb8e30b060796743d3901057
radek44413 <radek4441399@gmail.com>
2023-03-28 15:32:57 +0200

    asd

--- files/two-player.js
+++ files/two-player.js
@@ -1,11 +1,13 @@
-const ash_win = new Audio("audio/ash_win.mp3");
-const gary_win = new Audio("audio/gary_win.mp3");
+const ashWin = new Audio("audio/ash_win.mp3");
+const garyWin = new Audio("audio/gary_win.mp3");
 const remis = new Audio('audio/remis.mp3');
 const backgroundMusic = new Audio('audio/background.mp3');
 
 let whoseTurn;				//zmienna: string	zawiera informacje, kto wykonuje wych w danej turze
 let ashScore = 0;			//zmienna: liczba	wynik asha
 let garyScore = 0;			//zmienna: liczba	wynik garego
+let cardsImages;			//zmienna: tablica	tablica przechowujaca nazwy obrazow kart
+let cardNr;					//zmienna: tablica	tablica przechowujaca elementy kart (getElementById)
 let oneVisible = false;		//zmienna: bool		pozwala ustalic, czy jedna karta juz zostala odslonieta
 let turnCounter = 0;		//zmienna: liczba	zlicza liczbe wykonanych tur
 let visibleNr;				//zmienna: liczba	przechowuje numer odslonietej na poczatku tury karty 
@@ -14,61 +16,40 @@ let pairsLeft = 8;			//zmienna: liczba	przechowje liczbe pozostalych na planszy 
 let card;					//zmienna: obiekt	przechowuje element karty (getElementById)
 let opacityValue;			//zmienna: obiekt	przechowuje wartosc css opacity dla danego elementu karty (jesli wartosc wynosi 0, to karta zniknela z planszy)
 
-let playerAsh = document.getElementById("ash");
-let playerGary = document.getElementById("gary");
+document.getElementById("ash").addEventListener("click", () => generateBoard("ash"));
+document.getElementById("gary").addEventListener("click", () => generateBoard("gary"));
 
-playerAsh.addEventListener("click", function() {setFirstPlayer("ash")});
-playerGary.addEventListener("click", function() {setFirstPlayer("gary")});
+cardsImages = ["1.png", "1.png", "2.png", "2.png", "3.png", "3.png", "4.png", "4.png", "5.png", "5.png", "6.png", "6.png", "7.png", "7.png", "8.png", "8.png"];
+cardNr = new Array(16);
 
-function setFirstPlayer(p){
+shuffle(cardsImages);
+
+//funkcja generujaca poczatek rozgrywki
+const generateBoard = (player) =>{
 	backgroundMusic.play();
-	whoseTurn = p;
+	whoseTurn = player;
 	document.querySelector('.board').innerHTML = '<br/><div class="row"><div class="card" id="c0"></div><div class="card" id="c1"></div><div class="card" id="c2"></div><div class="card" id="c3"></div></div><div class="row"><div class="card" id="c4"></div><div class="card" id="c5"></div><div class="card" id="c6"></div><div class="card" id="c7"></div></div><div class="row"><div class="card" id="c8"></div><div class="card" id="c9"></div><div class="card" id="c10"></div><div class="card" id="c11"></div></div><div class="row"><div class="card" id="c12"></div><div class="card" id="c13"></div><div class="card" id="c14"></div><div class="card" id="c15"></div></div></div><div class="scoreCounter">Turn counter:  '+ turnCounter +'</div>'+ '</div><div class="scoreWhoseTurn">Whose Turn:  '+ whoseTurn +'</div>';
 	registerListeners();
 	if(whoseTurn === "ash"){
 		document.querySelector('.sidePlayerAsh').style.filter = 'brightness(100%)';
-		document.querySelector('.scoreWhoseTurn').innerHTML = 'Turn: ' + '<img src="img/ash_head.png" height="40">';
+		document.querySelector('.scoreWhoseTurn').innerHTML = 'Turn: ' + '<img src="img/ash_head.png" height="40"/>';
 	}
 	if(whoseTurn === "gary"){
 		document.querySelector('.sidePlayerGary').style.filter = 'brightness(100%)';
-		document.querySelector('.scoreWhoseTurn').innerHTML = 'Turn: ' + '<img src="img/gary_head.png" height="40">';
+		document.querySelector('.scoreWhoseTurn').innerHTML = 'Turn: ' + '<img src="img/gary_head.png" height="40"/>';
 	}
 }
-
-let cardsImages = ["1.png", "1.png", "2.png", "2.png", "3.png", "3.png", "4.png", "4.png", "5.png", 
-"5.png", "6.png", "6.png", "7.png", "7.png", "8.png", "8.png"];
-let c_ = new Array(16);
-
-function registerListeners(){
-
-	for(i=0; i<cardsImages.length; i++){
-		c_[i] = document.getElementById("c" + i);
-	}
-	c_[0].addEventListener("click", function() {revealCard(0);});
-	c_[1].addEventListener("click", function() {revealCard(1);}); 
-	c_[2].addEventListener("click", function() {revealCard(2);}); 
-	c_[3].addEventListener("click", function() {revealCard(3);}); 
-
-	c_[4].addEventListener("click", function() {revealCard(4);}); 
-	c_[5].addEventListener("click", function() {revealCard(5);}); 
-	c_[6].addEventListener("click", function() {revealCard(6);}); 
-	c_[7].addEventListener("click", function() {revealCard(7);}); 
-
-	c_[8].addEventListener("click", function() {revealCard(8);}); 
-	c_[9].addEventListener("click", function() {revealCard(9);}); 
-	c_[10].addEventListener("click", function() {revealCard(10);}); 
-	c_[11].addEventListener("click", function() {revealCard(11);}); 
-
-	c_[12].addEventListener("click", function() {revealCard(12);}); 
-	c_[13].addEventListener("click", function() {revealCard(13);}); 
-	c_[14].addEventListener("click", function() {revealCard(14);}); 
-	c_[15].addEventListener("click", function() {revealCard(15);}); 
+//funkcja rejestrujaca listenery
+const registerListeners = () =>{
+	cardsImages.forEach((_, i) => 
+		{cardNr[i] = document.getElementById("c" + i);});
+	cardNr.forEach((card, index) => 
+		{card.addEventListener("click", () => {revealCard(index);});});
 
 	document.querySelector('.left').innerHTML = '<div class="sidePlayerAsh"></div><div class="scoreAsh">Ash score: 0</div>';
 	document.querySelector('.right').innerHTML = '<div class="sidePlayerGary"></div><div class="scoreGary">Gary score: 0</div>';
 }
 
-shuffle(cardsImages);
 
 function shuffle(array) {
 
@@ -116,21 +97,20 @@ function revealCard(nr) {
 }
 
 //funkcja zaslaniajaca dwie karty wybrane w turze
-function restore2Cards(nr1, nr2) {
-
-	let card1 = document.querySelector('#c' + nr1);
-	card1.style.backgroundImage = 'url(img/background.png)';
-	card1.classList.add('card');
-	card1.classList.remove('cardA');
-  
-	let card2 = document.querySelector('#c' + nr2);
-	card2.style.backgroundImage = 'url(img/background.png)';
-	card2.classList.add('card');
-	card2.classList.remove('cardA');
-
+function restore2Cards(firstCard, secondCard) {
+	restoreCard(firstCard);
+	restoreCard(secondCard);
 	lock = false;
 	changeWhoseTurn();
 	updateStats();
+}
+
+//funkcja zaslaniajaca jedna karte
+function restoreCard(cardNr){
+	let cardToRestore = document.querySelector('#c' + cardNr);
+	cardToRestore.style.backgroundImage = 'url(img/background.png)';
+	cardToRestore.classList.add('card');
+	cardToRestore.classList.remove('cardA');
 }
 
 //funkcja usuwajaca z planszy pare identycznych kart
@@ -143,17 +123,19 @@ function hide2Cards(nr1, nr2){
 	if(pairsLeft === 0){
 		if(ashScore > garyScore) {
 			showResult("ash_win");
+			ashWin.play();
 			return;
 		}
 		if(ashScore === garyScore) {
-			showResult("gary_win");
+			showResult("remis");
+			remis.play();
 			return;
 		}
 		if(ashScore < garyScore) {
-			showResult("remis");
+			showResult("gary_win");
+			garyWin.play();
 			return;
 		}
-		lose.play();
 	}
 	lock = false;
 }
@@ -186,8 +168,9 @@ function updateStats(){
 }
 
 function showResult(winner){
-	document.querySelector('.board').innerHTML = '<img src="img/' + winner + '.gif"/><h1>Ash win!<br/>Done in ' + turnCounter + ' turns!';
+	if (winner === "remis") document.querySelector('.board').innerHTML = '<img src="img/' + winner + '.gif"/><h1>Remis!<br/>Done in ' + turnCounter + ' turns!';
+	if (winner === "ash_win") document.querySelector('.board').innerHTML = '<img src="img/' + winner + '.gif"/><h1>Ash win!<br/>Done in ' + turnCounter + ' turns!';
+	if (winner === "gary_win") document.querySelector('.board').innerHTML = '<img src="img/' + winner + '.gif"/><h1>Gary win!<br/>Done in ' + turnCounter + ' turns!';
 	document.querySelector('.left').innerHTML = '';
 	document.querySelector('.right').innerHTML = '';
-	winner.play();
 }
